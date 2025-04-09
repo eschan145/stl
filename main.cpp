a38c1db73d2067166243bc42a80b102a2ff7c67d
Ethan Chan <esamuelchan@gmail.com>
2025-04-09 11:08:09 -0700

    Test with null pointer dereference

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 
 struct Node {
+    int value = 1;
     Node() {
         std::cout << "Node created\n";
     }
@@ -15,4 +16,6 @@ struct Node {
 int main() {
     stl::ptr<Node> node1 = stl::make_ptr<Node>();
     stl::ptr<Node> node2 = stl::make_ptr<Node>();
+    stl::ptr<Node> node3 = nullptr;
+    node->value;
 }
